2f482a6c8e24ada3da0e9b36a430256d3c339291
Zack Kitzmiller <zackkitzmiller@gmail.com>
2026-08-30 00:07:23 -0500

    Fix install generator crashing on missing ActiveRecord::Generators
    next_migration_number referenced ActiveRecord::Generators::Base, which
    isn't loaded outside an ActiveRecord generator context. Use the
    Rails::Generators::Migration helper methods instead.

--- lib/generators/onetimer/install/install_generator.rb
+++ lib/generators/onetimer/install/install_generator.rb
@@ -11,7 +11,8 @@ module Onetimer
       source_root File.expand_path("templates", __dir__)
 
       def self.next_migration_number(dirname)
-        ActiveRecord::Generators::Base.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        ActiveRecord::Migration.next_migration_number(next_migration_number)
       end
 
       def create_migration_file
